c11a0e3825e37159be71401262132e64ca61b076
braullio <braulliovg@gmail.com>
2025-12-25 16:34:31 -0300

    git init

---
+++ src/test/java/com/github/braullio/codes/CodesApplicationTests.java
@@ -0,0 +1,15 @@
+package com.github.braullio.codes;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@Disabled("Contexto completo não necessário para testes unitários")
+class CodesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
